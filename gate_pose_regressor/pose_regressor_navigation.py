0e947d47697b4e71b8d75e642fbbdc3a7e54ea81
Rogerio Bonatti <rogeriobonatti@gmail.com>
2019-07-23 14:54:09 -0700

    new fiels for gate pose regressions

--- gate_pose_regressor/pose_regressor_navigation.py
+++ gate_pose_regressor/pose_regressor_navigation.py
@@ -9,6 +9,13 @@ import time
 import numpy as np
 import threading
 import pdb
+import gate_regressor
+
+import os,sys
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+airsim_path = os.path.join(curr_dir, '..', 'airsim')
+sys.path.insert(0, airsim_path)
+import setup_path
 
 
 def takeoff_with_moveOnSpline(client, takeoff_height=1.2, vel_max=15.0, acc_max=5.0):
@@ -21,6 +28,10 @@ if __name__ == "__main__":
     client.confirmConnection()
     client.enableApiControl(True, vehicle_name='drone_0')
     time.sleep(0.01)
+
+    img_resolution = 96
+    path_weights = '/home/rb/catkin_ws/src/AutonomousDrivingCookbook/AirSimE2EDeepLearning/output_reg2/regressor_model_100.ckpt'
+    gate_regressor = gate_regressor.GateRegressor()
 
     #todo: setup client and quadrotor properly for takeoff
 
