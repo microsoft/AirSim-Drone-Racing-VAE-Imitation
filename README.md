65c4819bd50a96fb3e5f789f0fffc59704d8daf5
Ratnesh Madaan <ratneshmadaan@gmail.com>
2019-07-24 11:25:06 -0700

    Create README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# drone_racing_hackathon
+
+Use binaries: https://github.com/madratman/drone_racing_hackathon/releases/tag/0.0.1
--- imitation_learning/imitator.py
+++ imitation_learning/imitator.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import torch.nn as nn
 
@@ -69,14 +71,30 @@ class Discriminator(nn.Module):
 
 class Imitator(object):
     """ Imitator implementation using adversarial CE loss"""
-    def __init__(self, task_name, state_dim, action_dim, lr=1e-4):
-        self.actor = Actor(state_dim, action_dim).to(device)
-        self.discriminator = Discriminator(state_dim, action_dim).to(device)
-        self.demonstrator = Demonstrations(task_name)
-        self.loss = nn.BCELoss()
-        self.actor_optim = torch.optim.Adam(self.actor.parameters(), lr=lr)
-        self.discriminator_optim = torch.optim.Adam(
-            self.discriminator.parameters())
+    def __init__(self, algo, task_name, state_dim, action_dim, lr=1e-4):
+        self.algo = algo
+        if self.algo == "adv":
+            self.actor = Actor(state_dim, action_dim).to(device)
+            self.discriminator = Discriminator(state_dim, action_dim).to(device)
+            self.demonstrator = Demonstrations(task_name)
+            self.loss = nn.BCELoss()
+            self.actor_optim = torch.optim.Adam(self.actor.parameters(), lr=lr)
+            self.discriminator_optim = torch.optim.Adam(
+                self.discriminator.parameters())
+            self.learn = self._learn_adversarial
+        elif algo == "bc":
+            from imitation_learning.models import MobileNetV2
+            self.demonstrator = Demonstrations(task_name)
+            self.actor = MobileNetV2()  # .to(device)
+            _state_dict = torch.load("imitation_learning/trained_models/mobilenet_v2.ptm")
+            self.actor.load_state_dict(_state_dict)
+            self.actor.classifier = nn.Linear(in_features=1280, out_features=4)
+            for params in self.actor.features.parameters():
+                params.requires_grad = False
+            self.loss = nn.MSELoss()
+            self.actor_optim = torch.optim.Adam(self.actor.parameters(), lr=lr)
+            self.learn = self._learn_bc
+
 
     def get_action(self, obs):
         """Get action based on imitator's current policy
@@ -90,15 +108,46 @@ class Imitator(object):
         obs = torch.FloatTensor(obs.reshape(1, -1)).to(device)
         return self.actor(obs).cpu().data.numpy().flatten()
 
-    def learn(self, num_iter, batch_size=256):
-        """Main routine to update the imitator model (actor, discriminator)
+    def _learn_bc(self, num_iter: int, batch_size: int=64) -> dict:
+        """ Learn an imitator model using behavior cloning
+
+        Args:
+            num_iter: -- Number of iterations
+            batch_size:
+
+        Returns: statistics
+
+        """
+        stats = {"loss_a": []}
+        for i in range(num_iter):
+            # Sample from the demonstrator
+            demo_states, demo_actions = self.demonstrator.sample(batch_size)
+            # demo_states = demo_state.to(device)
+            # demo_actions = demo_action.to(device)
+
+            # Get predictions from actor
+            actor_actions = self.actor(demo_states)
+
+            # Update actor
+            loss = self.loss(demo_actions, actor_actions)
+            self.actor_optim.zero_grad()
+            loss.backward()
+            self.actor_optim.step()
+            stats["loss_a"].append(loss.item())
+        return stats
+
+
+    def _learn_adversarial(self, num_iter: int, batch_size: int=256) -> dict:
+        """Learn an imitator model (actor, discriminator) using adversarial loss
         
-        Arguments:
+        Args:
             num_iter {int} -- Number of iterations to train
         
         Keyword Arguments:
             batch_size {int} -- Batch size (default: {256})
+        Returns: statistics
         """
+        stats = {"loss_d": [], "loss_a": []}
         for i in range(num_iter):
             # Sample from the demonstrator
             demo_state, demo_action = self.demonstrator.sample(batch_size)
@@ -125,9 +174,45 @@ class Imitator(object):
             self.discriminator_optim.zero_grad()
             loss.backward()
             self.discriminator_optim.step()
+            stats["loss_d"].append(loss.item())
 
             # Update Actor
             loss_actor = - self.discriminator(state, actor_action)
             self.actor_optim.zero_grad()
             loss_actor.mean().backward()
             self.actor_optim.step()
+            stats["loss_a"].append(loss.item())
+        return stats
+
+    def save(self, save_dir="imitation_learning/trained_models"):
+        """Save current model states to save_dir"""
+        os.makedirs(save_dir, exist_ok=True)
+        if self.algo == "adv":
+            torch.save(self.actor.state_dict(), f"{save_dir}/actor_adv.ptm")
+            torch.save(self.discriminator.state_dict(), f"{save_dir}/discriminator_adv.ptm")
+            print(f"Saved actor_adv.ptm & discriminator_adv.ptm")
+        elif self.algo == "bc":
+            torch.save(self.actor.state_dict(), f"{save_dir}/actor_bc.ptm")
+            print(f"Saved {save_dir}/actor_bc.ptm")
+
+    def load(self, load_dir="imitation_learning/trained_models"):
+        """Load model states from load_dir"""
+        if self.algo == "adv":
+            actor_model_file = f"{load_dir}/actor_adv.ptm"
+            discriminator_model_file = f"{load_dir}/discriminator_adv.ptm"
+            if os.path.isfile(actor_model_file) and os.path.isfile(
+                discriminator_model_file
+            ):
+                self.actor.load_state_dict(torch.load(actor_model_file))
+                self.discriminator.load_state_dict(torch.load(discriminator_model_file))
+                print("Loaded actor & discriminator model.")
+            else:
+                raise ValueError("Model files not found. Please check trained-models-dir")
+        if self.algo == "bc":
+            actor_model_file = f"{load_dir}/actor_bc.ptm"
+            if os.path.isfile(actor_model_file):
+                self.actor.load_state_dict(torch.load(actor_model_file))
+                print("Loaded actor_bc model.")
+            else:
+                raise ValueError("Model files not found. Please check trained-models-dir")
+
