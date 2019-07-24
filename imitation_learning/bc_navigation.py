5c7b47c880eed601171dea8c88b67b986ae34a33
Rogerio Bonatti <rogeriobonatti@gmail.com>
2019-07-24 16:18:59 -0700

    new bc models

---
+++ imitation_learning/bc_navigation.py
@@ -0,0 +1,68 @@
+from __future__ import division
+
+import os, sys
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+airsim_path = os.path.join(curr_dir, '..', 'airsim')
+sys.path.insert(0, airsim_path)
+import setup_path
+import airsim
+import airsim.types
+import airsim.utils
+import time
+import numpy as np
+import gate_regressor
+import utils_reg
+
+print(os.path.abspath(airsim.__file__))
+
+if __name__ == "__main__":
+    # set airsim client
+    client = airsim.MultirotorClient()
+    client.confirmConnection()
+    # client.simLoadLevel('Soccer_Field_Easy')
+    client.simLoadLevel('Soccer_Field_Medium')
+    time.sleep(2)
+    # should match the names in settings.json
+    drone_name = "drone_0"
+
+    client.enableApiControl(True, vehicle_name=drone_name)
+    time.sleep(0.01)
+    client.armDisarm(True, vehicle_name=drone_name)
+    time.sleep(0.01)
+    client.setTrajectoryTrackerGains(airsim.TrajectoryTrackerGains(), vehicle_name=drone_name)
+    time.sleep(0.01)
+
+    # utils_reg.MoveCheckeredGates(client)
+    utils_reg.RedGateSpawner(client)
+
+    # wait till takeoff complete
+    time.sleep(0.2)
+    time.sleep(0.05)
+    client.moveOnSplineAsync([airsim.Vector3r(0, 0, 10)], vel_max=15.0, acc_max=5.0, vehicle_name=drone_name).join()
+
+    time.sleep(1.0)
+
+    img_resolution = 96
+    vel_max = 15.0
+    acc_max = 5.00
+    path_weights = '/home/rb/catkin_ws/src/AutonomousDrivingCookbook/AirSimE2EDeepLearning/output_reg3/regressor_model_185.ckpt'
+    gate_regressor = gate_regressor.GateRegressor(img_resolution, path_weights)
+
+    while True:
+        image_response = client.simGetImages([airsim.ImageRequest('front_center_custom', airsim.ImageType.Scene, False, False)])[0]
+        img_1d = np.fromstring(image_response.image_data_uint8, dtype=np.uint8)  # get numpy array
+        img_rgb = img_1d.reshape(image_response.height, image_response.width, 3).astype(np.float32)  # reshape array to 4 channel image array H X W X 3
+        img_batch_1 = np.reshape(img_rgb, (-1,)+img_rgb.shape)
+        # print('size img = {}'.format(img_batch_1.shape))
+        cam_pos = image_response.camera_position
+        cam_orientation = image_response.camera_orientation
+        p_o_b = airsim.types.Pose(cam_pos, cam_orientation)
+        gate_pose = gate_regressor.predict_gate_pose(img_batch_1, p_o_b)
+        #todo: adjust spline points to .5m in front and behind gates after this works
+        print(gate_pose.position)
+        print('Before move spline')
+        client.moveByVelocityAsync()
+        client.moveOnSplineAsync([gate_pose.position], vel_max=vel_max, acc_max=acc_max, vehicle_name=drone_name)
+        client.moveByVelocityAsync()
+        print('Done move spline')
+        time.sleep(1.0)
---
+++ imitation_learning/utils_bc.py
@@ -0,0 +1,70 @@
+import airsim
+from airsim.utils import to_eularian_angles, to_quaternion
+import numpy as np
+from airsim.types import Pose, Vector3r, Quaternionr
+import nav_msgs.msg
+import geometry_msgs.msg
+import tf2_ros
+import tf_conversions
+import tf2_msgs
+import tf2_py
+import tf
+import tf.msg
+from tf import transformations
+import rospy
+import time
+
+
+def convert_vel_base2world(p_o_b, vx, vy, vz, v_yaw):
+    # transform relative vector from base frame to the world frame
+    m = geometry_msgs.msg.TransformStamped()
+    m.header.frame_id = 'world_frame'
+    m.header.stamp = rospy.Time(10)
+    m.child_frame_id = 'base_frame'
+    m.transform.translation.x = p_o_b.position
+    m.transform.rotation.x = p_o_b.orientation.x_val
+    m.transform.rotation.y = p_o_b.orientation.y_val
+    m.transform.rotation.z = p_o_b.orientation.z_val
+    m.transform.rotation.w = p_o_b.orientation.w_val
+    m.transform.translation.x = p_o_b.position.x_val
+    m.transform.translation.y = p_o_b.position.y_val
+    m.transform.translation.z = p_o_b.position.z_val
+
+    point = geometry_msgs.msg.PointStamped()
+    point.point.x = t_b_g.x_val
+    point.point.y = t_b_g.y_val
+    point.point.z = t_b_g.z_val
+    point.header.frame_id = 'base_frame'
+    point.header.stamp = rospy.Time(10)  # set an arbitrary time instance
+
+    t = tf.TransformerROS()
+    t.setTransform(m)
+
+    # finally we get the gate coord in world coordinates
+    point_converted = t.transformPoint('world_frame', point)
+    t_o_g = Vector3r(point_converted.point.x, point_converted.point.y, point_converted.point.z)  # now as a world coord vector
+
+    # create rotation of gate
+    # find vector t_b_g in world coordinates
+    t_b_g = t_o_g - p_o_b.position
+    phi_quad_ref = np.arctan2(t_b_g.y_val, t_b_g.x_val)
+    phi_gate = phi_quad_ref + phi_rel
+    q = transformations.quaternion_from_euler(phi_gate, 0, 0, axes='rzyx')
+    p_o_g = Pose(t_o_g, Quaternionr(q[0], q[1], q[2], q[3]))
+    return p_o_g
+
+
+def MoveCheckeredGates(client):
+    gate_names_sorted = sorted(client.simListSceneObjects("Gate.*"))
+    # gate_names_sorted_bad is ['Gate0', 'Gate10_21', 'Gate11_23', 'Gate1_3', 'Gate2_5', 'Gate3_7', 'Gate4_9', 'Gate5_11', 'Gate6_13', 'Gate7_15', 'Gate8_17', 'Gate9_19']
+    # number after underscore is unreal garbage
+    pose_far = Pose(Vector3r(0,0,1), Quaternionr())
+    for gate in gate_names_sorted:
+        client.simSetObjectPose(gate, pose_far)
+        # time.sleep(0.05)
+
+
+def RedGateSpawner(client):
+    for idx in range(10):
+        pose = Pose(Vector3r(10+idx*6, (np.random.random()-0.5)*5.0, 10.0), Quaternionr(0.0, 0.0, 0.707, 0.707))
+        client.simSpawnObject("gate_"+str(idx), "RedGate16x16", pose, 1.5)
